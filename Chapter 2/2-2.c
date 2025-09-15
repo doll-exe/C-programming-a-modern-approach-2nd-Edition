70849d23c69b8b14292b45f1f27e8743a5c76fcc
glassrouter <rincess@snu.ac.kr>
2025-09-15 16:38:30 +0900

    Update 2-2.c

--- Chapter 2/2-2.c
+++ Chapter 2/2-2.c
@@ -3,12 +3,20 @@ Write the fraction 4/3 as 4.0f/3.0f (Try writing it as 4/3. What happens?)
 Hint: C doesn't have an exponentiation operator, so you'll need to multiply r by itself twice to compute r^3.*/
 
 #include <stdio.h>
-#include <math.h>
-#define USE_MATH_DEFINES
+#define PI 3.14159f
 
 int main(void) {
     float r = 10.0f;
-    float volume = (4.0f / 3.0f) *M_PI * r * r * r;
+    float volume = (4.0f / 3.0f) *PI * r * r * r;
     printf("%.2f\n", volume);
     return 0;
 }
+
+/*
+While solving I was wondering what's the difference between:
+    float r = 10; int → float
+    float r = 10.0; double → float (warning possible)
+    float r = 10.0f; float → float (cleanest)
+
+Also the difference between 4/3 and 4.0f/3.0f is that second option is more precise and we do not lose correct values
+*/
