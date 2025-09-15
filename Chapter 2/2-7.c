82c28ea30628c67e172d47051656ea0bffbbde1a
glassrouter <rincess@snu.ac.kr>
2025-09-16 00:48:19 +0900

    Update 2-7.c

--- Chapter 2/2-7.c
+++ Chapter 2/2-7.c
@@ -34,23 +34,3 @@ int main(void) {
     }
     return 0;
 }
-
-/*
-First I decided to solve this problem in Python because I wanted to see how to approach it
-(and check if I havent forgotten Python skills AND I DIDNT!! This problem is not hard but
-I was just happy by how fast I solved it)
-
-7.py
-
-x = int(input())
-dollars = [20, 10, 5, 1]
-dummy = 0
-for i in range (len(dollars)):
-    if x//dollars[i]!=0:
-        dummy = x//dollars[i]
-        x-=dummy*dollars[i]
-        print(f"${dollars[i]} bills: {dummy}")
-    else:
-        print(f"${dollars[i]} bills: 0")
-
-*/
