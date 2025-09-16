254b7a9176bc7072dc89751f1f5ee1860283835a
glassrouter <rincess@snu.ac.kr>
2025-09-16 15:42:36 +0900

    Create 3-1.c

---
+++ Chapter 3/3-1.c
@@ -0,0 +1,15 @@
+/*
+Write a program that accepts a date from the user in the form mm/dd/yyy and then displays it in the form yyymmdd:
+
+Enter a date (mm/dd/yyyy): 2/17/2011
+You entered the date 20110217
+*/
+#include <stdio.h>
+
+int main(void) {
+    int month, day, year;
+    printf("Enter a date (mm/dd/yyyy):");
+    scanf("%d/%d/%d",&month, &day, &year);
+    printf("You entered the date %d%02d%02d", year, month, day);
+    return 0;
+}
