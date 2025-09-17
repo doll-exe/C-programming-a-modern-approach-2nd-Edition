de751bf512751f9b483426210da6a65d15e62ea7
glassrouter <rincess@snu.ac.kr>
2025-09-17 17:55:06 +0900

    Create 3-3.py

---
+++ Chapter 3/3-3.py
@@ -0,0 +1,39 @@
+'''Books are identified by an International Standard Book Number (ISBN). ISBNs assigned after January 1, 
+2007 contain 13 digits, arranged in five groups, such as 978-0-393-97950-3. (Older ISBNs use 10 digits.) 
+The first group (the GSI prefix) is currently either 978 or 979. The group identifier specifies 
+the language or country of origin (for example, 0 and 1 are used in English-speaking countries). 
+The publisher code identifies the publisher (393 is the code for W. W. Norton). The item number is assigned by 
+the publisher to identify a specific book (97950 is the code for this book). An ISBN ends with a check digit 
+that's used to verify the accuracy of the preceding digits. 
+Write a program that breas down an ISBN entered by the user:
+-----------------------------------------------------------------------------------------------------------
+Enter ISBN: 978-0-393-97950-3
+GSI prefix: 978
+Group identifier: 0
+Publisher code: 393
+Item number: 97950
+Check digit: 3
+-----------------------------------------------------------------------------------------------------------
+Note: The number of digits in each group may vary: you can't assume that groups have the lengths shown in this example. 
+Test your program with actual ISBN values (usually found on the back cover of a book and on the copyright page).'''
+
+isbn = input()
+labels = ["GSI prefix", "Group identifier", "Publisher code", "Item number", "Check digit"]
+parts = isbn.split("-") 
+mydic = {}
+for i in range(len(labels)):
+    mydic[labels[i]] = parts[i]
+for key, value in mydic.items():
+    print(f"{key}: {value}")
+
+'''
+NOTES:
+for key in mydic:
+    print(key, mydic[key]) - prints only keys
+
+for key in mydic.keys():
+    print(key) - explicitly loops through keys
+    
+for value in mydic.values():
+    print(value) 
+'''
