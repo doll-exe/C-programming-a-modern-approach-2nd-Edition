18d56b49b89ae14c57b126ba5b92216b272bc7da
glassrouter <rincess@snu.ac.kr>
2025-09-18 13:41:55 +0900

    Create 3-4.c

---
+++ Chapter 3/3-4.c
@@ -0,0 +1,15 @@
+/*
+Write a program that prompts the user to enter a telephone number in the form (xxx) xxx-xxxx 
+and then displays the number in the form xxx.xxx.xxxx:
+
+Enter phone number [(xxx) xxx-xxxx]: (404) 817-6900
+You entered 404.817.6900
+*/
+
+#include <stdio.h>
+int main(void) {
+    int num1,num2,num3;
+    printf("Enter phone number [(xxx) xxx-xxxx]: ");
+    scanf("(%3d) %3d-%4d", &num1, &num2, &num3);
+    printf("You entered %d.%d.%d", num1, num2, num3);
+}
