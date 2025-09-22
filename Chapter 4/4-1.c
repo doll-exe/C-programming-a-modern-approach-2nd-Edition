785ab67d629c7bc7bb370072aadbc02e8407dc54
glassrouter <rincess@snu.ac.kr>
2025-09-22 17:02:41 +0900

    Create 4-1.c

---
+++ Chapter 4/4-1.c
@@ -0,0 +1,18 @@
+/*
+Write a program that asks the user to enter a two-digit number, then prints the number with its digits reversed. A session with the program should have the following appearance:
+
+Enter a two-digit number: 28
+The reversal is: 82
+Read the number using %d, then break it into two digits. Hint: If n is an integer, then n % 10 is the last digit in n and n / 10 is n with the last digit removed.
+*/
+#include <stdio.h>
+
+int main (void) {
+    int num, n1, n2;
+    printf("Enter a two-digit number: ");
+    scanf("%2d", &num);
+    n1 = num/10;
+    n2 = num%10;
+    printf("The reversal is: %d%d", n1, n2);
+    return 0;
+}
