1938f561a3ca7df2ba9edf2d01d049baf86a9988
glassrouter <rincess@snu.ac.kr>
2025-10-21 12:07:59 +0900

    Update 5.c

--- Chapter 11/Exercises/5.c
+++ Chapter 11/Exercises/5.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+
 void split_time(long total_sec, int *hr, int *min, int *sec);
+
 int main(void) {
     int h,m,s;
     long total;
-    scanf("How many seconds past midnight?: %ld", &total);
+    printf("How many seconds?: ");
+    scanf("%ld", &total);
     int *hr=&h, *min=&m, *sec=&s;
-    split_time(total, hr, min, sec)
-    
+    split_time(total, hr, min, sec);
+    printf("%d:%d:%d after midnight\n", *hr, *min, *sec);
     return 0;
 }
 void split_time(long total_sec, int *hr, int *min, int *sec){
     *hr = total_sec/3600;
-    //stopped here
+    *min = (total_sec - *hr*3600)/60;
+    *sec = (total_sec - *hr*3600)%60;
+    *hr-=24;
+    if (*hr<=0) *hr=0;
 }
