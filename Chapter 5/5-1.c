4b53901803a111aec61b4a61c8a08f174ab24bcb
glassrouter <rincess@snu.ac.kr>
2025-10-28 15:19:07 +0900

    Update 5-1.c

--- Chapter 5/5-1.c
+++ Chapter 5/5-1.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+int main(void) {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num>=0 && num<=9) printf("The number %d has 1 digit", num);
+    else if (num>=10 && num<=99) printf("The number %d has 2 digits", num);
+    else if (num>=100 && num<=999) printf("The number %d has 3 digits", num);
+    else if (num>999) printf("The number is too big.");
+    return 0;
+}
